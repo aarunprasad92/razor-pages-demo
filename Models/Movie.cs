4f05d1551ca8d6e1af089e62e9766035207aad89
Arun Prasad A <aarunprasad92@gmail.com>
2021-10-16 23:03:01 +0530

    Added new filed rating to movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -12,10 +12,12 @@ public class Movie
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        
+
         public string Genre { get; set; }
 
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        public string Rating { get; set; }
     }
 }
